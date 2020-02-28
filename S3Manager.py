b5b406492d8a4e47ac8bf402d97acb19399fd795
command7 <vijay_saravanan@outlook.com>
2020-02-27 19:03:54 -0500

    Feat: Store file paths as instance variables and add getter methods for them

--- S3Manager.py
+++ S3Manager.py
@@ -8,9 +8,14 @@ class S3Manager:
     def __init__(self):
         self.bucket_details = dict()
 
+        self.parse_configurations()
+
         self.s3_resource = boto3.resource('s3')
 
-        self.parse_configurations()
+        self.title_basics_paths = list()
+        self.title_principals_paths = list()
+        self.title_ratings_paths = list()
+        self.name_basics_paths = list()
 
     def get_principals_bucket(self):
         return self.bucket_details["title_principals"]
@@ -23,6 +28,18 @@ class S3Manager:
 
     def get_ratings_bucket(self):
         return self.bucket_details["title_ratings"]
+
+    def get_basics_paths(self):
+        return self.title_basics_paths
+
+    def get_principals_paths(self):
+        return self.title_principals_paths
+
+    def get_ratings_paths(self):
+        return self.title_ratings_paths
+
+    def get_names_paths(self):
+        return self.name_basics_paths
 
     def parse_configurations(self):
         conf_parser = configparser.ConfigParser()
@@ -38,3 +55,13 @@ class S3Manager:
         self.bucket_details["name_basics"] = conf_parser["S3_Bucket"]["NAME_BASICS"]
         self.bucket_details["title_principals"] = conf_parser["S3_Bucket"]["TITLE_PRINCIPALS"]
         self.bucket_details["title_ratings"] = conf_parser["S3_Bucket"]["TITLE_RATINGS"]
+
+    def list_all_contents(self):
+        basics_bucket = self.s3_resource.Bucket(self.get_basics_bucket())
+        for each_item in basics_bucket.objects.all():
+            print(each_item.key)
+
+
+if __name__ == "__main__":
+    s3_manager = S3Manager()
+    s3_manager.list_all_contents()
