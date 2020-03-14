f014c5fe1b648d110e4bdecfe7abc72f6a28373d
command7 <vijay_saravanan@outlook.com>
2020-03-14 15:30:47 -0400

    Feat: Create transform_bridge_dimensions() that constructs bridge dimensions

--- Imdb_Etl/ETLManager.py
+++ Imdb_Etl/ETLManager.py
@@ -149,6 +149,57 @@ class ETLManager:
         self.dynamo_db_manager.update_media_details_starting_sk(last_media_details_sk)
         return media_details_dim
 
+    def transform_bridge_dimensions(self):
+        member_dim_initial_sk = self.dynamo_db_manager.get_media_member_starting_sk()
+        member_bridge_initial_sk = self.dynamo_db_manager.get_member_bridge_starting_sk()
 
+        joined_df = self.basics_data.join(self.principals_data,
+                                          self.basics_data.tb_tconst == self.principals_data.tp_tconst,
+                                          how="left")
+        joined_df = joined_df.join(self.names_data,
+                                   joined_df.tp_nconst == self.names_data.nb_nconst,
+                                   how="left").sort(F.asc("tb_tconst"),
+                                                    F.asc("tp_ordering"))
+        member_window = Window.orderBy('tp_tconst')
+        media_member_dim = joined_df.withColumn("media_member_key",
+                                                F.row_number().over(member_window) + member_dim_initial_sk) \
+            .select(F.col("media_member_key"),
+                    F.col("tp_nconst").alias("member_id"),
+                    F.col("nb_primaryname").alias("primary_name"),
+                    F.col("tp_job").alias("job_title"),
+                    F.col("tp_category").alias("job_category"),
+                    F.col("nb_birthyear").alias("birth_year"),
+                    F.col("nb_deathyear").alias("death_year"),
+                    F.col("nb_primaryprofession").alias("primary_profession"))
 
+        media_member_bridge = joined_df.select(F.col("tb_tconst"),
+                                               F.col("tp_nconst"),
+                                               F.col("tp_ordering"))
 
+        media_member_bridge = media_member_bridge.join(media_member_dim,
+                                                       media_member_bridge.tp_nconst == media_member_dim.member_id,
+                                                       how="inner") \
+            .sort(F.asc("tb_tconst"),
+                  F.asc("media_member_key")) \
+            .select(F.col("tb_tconst").alias("media_member_group_key"),
+                    F.col("media_member_key"))
+
+        bridge_window = Window.orderBy(media_member_bridge.media_member_group_key)
+        media_member_bridge = media_member_bridge\
+            .withColumn("rank", F.dense_rank().over(bridge_window) + member_bridge_initial_sk)\
+            .select(F.col("rank").alias("media_member_group_key"),
+                    F.col("media_member_key"))
+
+        last_member_bridge_starting_sk = media_member_bridge\
+            .sort(F.desc("media_member_group_key"))\
+            .first().media_member_group_key
+        last_media_member_starting_sk = media_member_dim\
+            .sort(F.desc("media_member_key"))\
+            .first().media_member_key
+        self.dynamo_db_manager.update_media_member_starting_sk(last_media_member_starting_sk)
+        self.dynamo_db_manager.update_member_bridge_starting_sk(last_member_bridge_starting_sk)
+
+        media_member_bridge.show()
+        media_member_dim.show()
+
+        return media_member_dim, media_member_bridge
