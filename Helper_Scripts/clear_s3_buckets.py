3efb7463e2e7198094cb4b13994c5adb8e2c34b8
command7 <vijay_saravanan@outlook.com>
2020-04-05 20:29:14 -0400

    Refactor: Change bucket name in clear_s3_buckets.py

--- Helper_Scripts/clear_s3_buckets.py
+++ Helper_Scripts/clear_s3_buckets.py
@@ -12,11 +12,11 @@ s3_resource = boto3.resource("s3",
                              aws_access_key_id=s3_access_key,
                              aws_secret_access_key=s3_secret_key)
 
-title_basics_bucket = s3_resource.Bucket('imdbtitlebasics')
-title_principals_bucket = s3_resource.Bucket('imdbtitleprincipals')
-title_ratings_bucket = s3_resource.Bucket('imdbtitleratings')
-name_basics_bucket = s3_resource.Bucket('imdbnamebasics')
-title_episodes_bucket = s3_resource.Bucket("imdbtitleepisodes")
+title_basics_bucket = s3_resource.Bucket('imdbtitlebasicsz')
+title_principals_bucket = s3_resource.Bucket('imdbtitleprincipalsz')
+title_ratings_bucket = s3_resource.Bucket('imdbtitleratingsz')
+name_basics_bucket = s3_resource.Bucket('imdbnamebasicsz')
+title_episodes_bucket = s3_resource.Bucket("imdbtitleepisodesz")
 
 title_basics_bucket.objects.all().delete()
 title_principals_bucket.objects.all().delete()
